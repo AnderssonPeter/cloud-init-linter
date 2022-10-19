3fd50f3252dc06c4ce9a79099490559e729f4224
AnderssonPeter <1308309+AnderssonPeter@users.noreply.github.com>
2022-10-19 21:37:06 +0200

    Added exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 readarray -t config_files < <(ls cloud-init*.{yml,yaml} -1q 2> /dev/null)
+had_errors=0
 for config_file in "${config_files[@]}"; do
     result="$(cloud-init devel schema --config-file $config_file 2>&1)"
     if [[ "$?" -eq 0 ]]
@@ -8,5 +9,8 @@ for config_file in "${config_files[@]}"; do
         echo "$result"
     else
         echo "::error file=$config_file::$result"
+        had_errors=1
     fi
 done
+
+exit $had_errors
